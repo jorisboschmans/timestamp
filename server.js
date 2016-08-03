d2cdd55faa326e8d2d6c7ef00bd04b766280c9b1
Joris Boschmans <jorisboschmans@hotmail.com>
2016-08-03 18:04:38 +0200

    Fixing hello world

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ var express = require("express");
 var app = express();
 
 app.get("/", function(req, res){
+	//var date = req.params.date;
 	res.end("Hello World!");
 });
 
-app.listen(3000, function(){
+app.listen(process.env.PORT, function(){
 	console.log("App is listening on port 3000");
 });
