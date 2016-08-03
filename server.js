23380bb00a1d07e082c678a81c648847465ad37e
Joris Boschmans <jorisboschmans@hotmail.com>
2016-08-03 18:33:30 +0200

    finished Timestamp App

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 var express = require("express");
 var app = express();
 
-app.get("/", function(req, res){
-	//var date = req.params.date;
-	res.end("Hello World!");
+app.get("/:date", function(req, res){
+	var date = req.params.date;
+
+	if (date.match(/^[0-9]{10}$/)) // UNIX input
+		date = new Date(+date * 1000);
+	else
+		date = new Date(date);
+
+	var text = {
+		unix: date.getTime(),
+		natural: date.toDateString()
+	}
+
+	res.end(JSON.stringify(text));
 });
 
-app.listen(process.env.PORT, function(){
-	console.log("App is listening on port 3000");
+app.listen(process.env.PORT || 3000, function(){
+	console.log("App is listening...");
 });
