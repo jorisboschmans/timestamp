f05f126aa89d57a7bff4b0ed0c324609cc2075f0
Joris Boschmans <jorisboschmans@hotmail.com>
2016-08-03 18:52:14 +0200

    sets properties to null when incorrect date 2

--- server.js
+++ server.js
@@ -1,18 +1,47 @@
 var express = require("express");
 var app = express();
 
+function numberToMonth(number){
+	switch (number){
+		case 0:
+			return "January";
+		case 1:
+			return "February";
+		case 2:
+			return "March";
+		case 3:
+			return "April";
+		case 4:
+			return "May";
+		case 5:
+			return "June";
+		case 6:
+			return "July";
+		case 7:
+			return "August";
+		case 8:
+			return "September";
+		case 9:
+			return "October";
+		case 10:
+			return "November";
+		case 11:
+			return "December";
+	}
+}
+
 app.get("/:date", function(req, res){
 	var date = req.params.date;
 
-	if (date.match(/^[0-9]{10}$/)) // UNIX input
+	if (date.match(/^[0-9]+$/)) // UNIX input
 		date = new Date(+date * 1000);
 	else
 		date = new Date(date);
 
-	if (date){
+	if (date.getTime()){
 		var text = {
 			unix: date.getTime(),
-			natural: date.toDateString()
+			natural: numberToMonth(date.getMonth()) + " " + date.getDate() + ", " + date.getFullYear()
 		};
 	} else {
 		var text = {
